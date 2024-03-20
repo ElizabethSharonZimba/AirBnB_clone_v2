6cb456dfbead3ae49ab955a18a5bd8d2ff82ffe5
ElizabethSharonZimba <sharonzimba95@gmail.com>
2024-03-20 08:30:17 -0700

    v2

---
+++ amenity.py
@@ -0,0 +1,18 @@
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Table, ForeignKey
+from sqlalchemy.orm import relationship
+
+class Amenity(BaseModel, Base):
+    """
+    Amenity class for storing amenity information.
+    """
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
+
+    place_amenities = relationship("Place", secondary="place_amenity", viewonly=False)
+
+place_amenity = Table('place_amenity', Base.metadata,
+                      Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
+                      Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False)
+                      )
+
